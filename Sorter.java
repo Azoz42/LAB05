6842c09d3d71fc861e8bcb96345b341d830615f6
Azoz42 <you@example.com>
2022-10-14 01:59:59 +0300

    selectionSort(Array)

--- Sorter.java
+++ Sorter.java
@@ -1,16 +1,21 @@
 public class Sorter {
-    public static void insertionSort(int array[]) {  
-        int n = array.length;  
-        for (int j = 1; j < n; j++) {  
-            int key = array[j];  
-            int i = j-1;  
-            while ( (i > -1) && ( array [i] > key ) ) {  
-                array [i+1] = array [i];  
-                i--;  
-            }  
-            array[i+1] = key;  
+    public static void main(String[] args) {
+
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;// searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
         }
 
-    } 
+    }
 
 }
